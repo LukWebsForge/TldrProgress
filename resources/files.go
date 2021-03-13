c2929767ae0297b955c976f7b81b4e6c99f3ef85
LukBukkit <luk.bukkit@gmail.com>
2021-03-13 14:41:04 +0100

    Cleans the 'static' folder when coping the assets

--- resources/files.go
+++ resources/files.go
@@ -9,12 +9,24 @@ import (
 
 const defaultFileMask = 0740
 const baseFSDir = "build"
+const staticDir = "static"
 
 //go:embed build
 var f embed.FS
 
 // Writes all static asset files from the 'resources/build' folder (now in the binary) to the given folder
 func WriteTo(ospath string) error {
+
+	// Removes the 'static' directory to prevent old hashed asset files from piling up
+	staticDirPath := filepath.Join(ospath, staticDir)
+	if _, err := os.Stat(staticDirPath); !os.IsNotExist(err) {
+		err := os.RemoveAll(staticDirPath)
+		if err != nil {
+			return err
+		}
+	}
+
+	// Copies all files from this binary to the disk
 	return fs.WalkDir(f, baseFSDir, func(fspath string, d fs.DirEntry, err error) error {
 		relPath, err := filepath.Rel(baseFSDir, filepath.FromSlash(fspath))
 		fPath := filepath.Join(ospath, relPath)
