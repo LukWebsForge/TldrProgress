2aefa14269488bded79f8a3232ecbcb107c29514
LukBukkit <luk.bukkit@gmail.com>
2021-06-05 16:53:25 +0200

    Use an enum for the theme type

--- resources/src/IconActionTheme.tsx
+++ resources/src/IconActionTheme.tsx
@@ -1,17 +1,17 @@
 import { useContext } from 'react'
 import { Tooltip } from '@geist-ui/react'
 import { Moon, Sun } from '@geist-ui/react-icons'
-import { ThemeTypeContext } from './ThemeType'
+import { ThemeType, ThemeTypeContext } from './ThemeType'
 
 const IconActionTheme = (props: { side?: boolean }) => {
   const { themeType, setThemeType } = useContext(ThemeTypeContext)
 
   const placement = props.side ? 'left' : 'top'
   const icon =
-    themeType === 'light' ? (
-      <Moon className="cursor-pointer" size={28} onClick={() => setThemeType('dark')} />
+    themeType === ThemeType.Light ? (
+      <Moon className="cursor-pointer" size={28} onClick={() => setThemeType(ThemeType.Dark)} />
     ) : (
-      <Sun className="cursor-pointer" size={28} onClick={() => setThemeType('light')} />
+      <Sun className="cursor-pointer" size={28} onClick={() => setThemeType(ThemeType.Light)} />
     )
 
   return (
--- resources/src/ThemeType.tsx
+++ resources/src/ThemeType.tsx
@@ -1,30 +1,35 @@
 import { createContext, PropsWithChildren, useContext, useEffect, useState } from 'react'
 
+enum ThemeType {
+  Light = 'light',
+  Dark = 'dark',
+}
+
 const ThemeTypeContext = createContext<{
-  themeType: string
-  setThemeType: (themeType: string) => void
+  themeType: ThemeType
+  setThemeType: (themeType: ThemeType) => void
 }>({
-  themeType: 'light',
+  themeType: ThemeType.Light,
   setThemeType: () => {},
 })
 
 const ThemeTypeProvider = (props: PropsWithChildren<{}>) => {
-  const [themeType, setThemeType] = useState('light')
+  const [themeType, setThemeType] = useState(ThemeType.Light)
   const webStorageThemeTypeKey = 'theme-type'
 
   // Fetch the user preference using the Web Storage API or determine the system theme
   useEffect(() => {
     const stored = localStorage.getItem(webStorageThemeTypeKey)
-    if (stored === 'light' || stored === 'dark') {
+    if (stored === ThemeType.Light || stored === ThemeType.Dark) {
       setThemeType(stored)
     } else if (window && typeof window.matchMedia === 'function') {
       // Inspired by: https://github.com/xcv58/use-system-theme/blob/master/src/index.tsx
       const isDark = window.matchMedia('(prefers-color-scheme: dark)').matches
-      setThemeType(isDark ? 'dark' : 'light')
+      setThemeType(isDark ? ThemeType.Dark : ThemeType.Light)
     }
   }, [])
 
-  const setPersistentThemeType = (themeType: string) => {
+  const setPersistentThemeType = (themeType: ThemeType) => {
     setThemeType(themeType)
     localStorage.setItem(webStorageThemeTypeKey, themeType)
   }
@@ -41,4 +46,4 @@ const useThemeType = () => {
   return themeType
 }
 
-export { ThemeTypeContext, ThemeTypeProvider, useThemeType }
+export { ThemeType, ThemeTypeContext, ThemeTypeProvider, useThemeType }
