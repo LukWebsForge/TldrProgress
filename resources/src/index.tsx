66c642fe598842592ecbaad467322ea84226f086
LukBukkit <luk.bukkit@gmail.com>
2023-09-04 16:39:56 +0200

    Uses the createRoot method of React 18

--- resources/src/index.tsx
+++ resources/src/index.tsx
@@ -1,15 +1,10 @@
-import { StrictMode } from 'react'
-import * as ReactDOM from 'react-dom'
 import { App } from './App'
+import { createRoot } from 'react-dom/client'
 
 // Learn more about TypeScript + React:
 // https://2ality.com/2018/04/type-notation-typescript.html
 // https://github.com/typescript-cheatsheets/react#section-1-setup-typescript-with-react
 // https://create-react-app.dev/docs/adding-typescript/
 
-ReactDOM.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-  document.querySelector('#root'),
-)
+const root = createRoot(document.getElementById('root')!)
+root.render(<App />)
